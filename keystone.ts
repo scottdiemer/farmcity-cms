5788dba68669bea7851c6231f666fb25f5dc7ace
scottdiemer <sdiemer@cfl.rr.com>
2022-02-01 17:09:10 -0500

    added cors to server config

--- keystone.ts
+++ keystone.ts
@@ -21,6 +21,7 @@ export default withAuth(
   config({
     server: {
       port: 5000,
+      cors: { origin: ['http://localhost:3000'], credentials: true},
     },
     // the db sets the database provider - we're using sqlite for the fastest startup experience
     db: {
