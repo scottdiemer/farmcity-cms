bbc4846ed1ff9639b6da65205b966e690d22558a
scottdiemer <sdiemer@cfl.rr.com>
2022-03-13 15:56:45 -0400

    Configuation changes

--- keystone.ts
+++ keystone.ts
@@ -27,7 +27,7 @@ export default withAuth(
     session: statelessSessions({
       secret: process.env.SESSION_SECRET,
       maxAge: process.env.MAX_AGE,
-      secure: false,
+      secure: true,
 
     }),
     // the db sets the database provider - we're using sqlite for the fastest startup experience
