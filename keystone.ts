e5f159993732c7f1da80b80e5416b7e22ff4ce96
scottdiemer <sdiemer@cfl.rr.com>
2022-03-20 16:36:48 -0400

    Added port to cors origin

--- keystone.ts
+++ keystone.ts
@@ -28,7 +28,7 @@ export default withAuth(
   config({
     server: {
       port: 5000,
-      cors: { origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://farmcityfeed.com', 'http://localhost:5000'], credentials: true},
+      cors: { origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://farmcityfeed.com:443', 'http://localhost:5000'], credentials: true},
     },
     // the db sets the database provider - we're using sqlite for the fastest startup experience
     db: {
