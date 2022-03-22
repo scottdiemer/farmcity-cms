ee2139e650c8012c6d4090b6d0a3ed0367e3bed2
scottdiemer <sdiemer@cfl.rr.com>
2022-03-22 10:10:31 -0400

    Hoping this fixes cors

--- keystone.ts
+++ keystone.ts
@@ -28,7 +28,7 @@ export default withAuth(
   config({
     server: {
       port: 5000,
-      cors: { origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://farmcityfeed.com'], credentials: true, methods: "POST", preflightContinue: false, optionsSuccessStatus: 204},
+      cors: true,
     },
     // the db sets the database provider - we're using sqlite for the fastest startup experience
     db: {
